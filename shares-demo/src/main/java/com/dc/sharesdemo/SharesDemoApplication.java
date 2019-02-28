87ff837cfa11c4af34fc3f8e136b405cd2d04172
danchellee <583746487@qq.com>
2019-02-28 15:39:56 +0800

    project

---
+++ shares-demo/src/main/java/com/dc/sharesdemo/SharesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dc.sharesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesDemoApplication.class, args);
+    }
+
+}
